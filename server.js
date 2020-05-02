8a3abc85851f62bc8d44bcf060995852cdc6bad4
Oprea Ghizela Maria <opreaghizelamaria@yahoo.com>
2020-05-02 18:04:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -171,8 +171,8 @@ io.on('connection', function(socket) {
 });
 
 //android and server comunication
-server.listen(3000, () => {
- console.log("Server running on port 3000");
+server.listen(8080, () => {
+ console.log("Server running on port 8080");
 });
 
 
